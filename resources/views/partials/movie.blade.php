542a663982ce325c36086e5ca8738016c27ae27f
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-27 19:12:56 +0200

    esercizio completo senza css

--- resources/views/partials/movie.blade.php
+++ resources/views/partials/movie.blade.php
@@ -1,10 +1,10 @@
-
 <div class="col h-100">
     <div class="card bg-dark">
-        <img src=" {{ $card['thumb'] }} " alt="" srcset="" class="card-img w-100">
-        <div class="title">
-            <h6 class="text-uppercase text-white p-2 fw-bold"> {{ $card['title'] }} </h6>
-        </div>
+        <a href="{{route('cards.show', $card['id'])}} " class="text-decoration-none">
+            <img src=" {{ $card['thumb'] }} " alt="" srcset="" class="card-img w-100">
+            <div class="title">
+                <h6 class="text-uppercase text-white p-2 fw-bold"> {{ $card['title'] }} </h6>
+            </div>
+        </a>
     </div>
 </div>
-
